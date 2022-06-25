6bc505662088aef3bbc0d98e14b2c62adc8e4e74
PrashamTrivedi <PrashamTrivedi@users.noreply.github.com>
2022-06-25 18:30:57 +0530

    Fixing index.js

--- index.js
+++ index.js
@@ -2,13 +2,10 @@
 import {ApolloServer} from 'apollo-server'
 import typeDefs from "./typedefs.js"
 import resolvers from "./resolvers.js"
-import {authorByBookIdLoader} from "./author.js"
-
+import loaders from './loader.js'
 const server = new ApolloServer({
     typeDefs, resolvers, csrfPrevention: true, context: {
-        loaders: {
-            findAuthorsByBookIdsLoader: authorByBookIdLoader(),
-        }
+        loaders: loaders()
     }
 })
 
